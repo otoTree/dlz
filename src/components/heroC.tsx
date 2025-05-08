790600a626c0e8804221cfdcb7d791338ff47ee2
Jasons-Manager <zhangze742@gmail.com>
2025-05-08 18:56:20 +0800

    Update heroC.tsx

--- src/components/heroC.tsx
+++ src/components/heroC.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         playsInline
         className="absolute inset-0 w-full h-full object-cover"
       >
-        <source src="/images/xuanchuan.mp4" type="video/mp4" />
+        <source src="http://cloud.video.taobao.com/play/u/939067861/p/2/e/6/t/1/358386696532.mp4" type="video/mp4" />
       </video>
       <div className="absolute inset-0 bg-black/30" />
       <div className="container-wrapper relative z-10 flex items-end h-full pb-24">
